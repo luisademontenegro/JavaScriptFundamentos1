e8d0d6d1ed1480f95d63dae8fc463c9e0433b2e6
Luísa de Montenegro <luisademontenegro@hotmail.com>
2022-01-08 02:15:46 -0300

    primeiro commit

---
+++ type-number.js
@@ -0,0 +1,30 @@
+//Tipo Number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatemetica = primeiroNumero + segundoNumero;
+
+console.log("Esta é a primeira operação matemática do curso:");
+console.log(operacaoMatemetica);
+
+
+//Ponto flutuante
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const novaOperacao = numeroPontoFlutuante + pontoFlutuanteSemZero;
+
+console.log("A o número a seguir é resultado de um cálculo utilizando ponto flutuante:");
+console.log(novaOperacao);
+
+//NaN - not a number
+
+console.log("Abaixo, o resultado de uma operação entre uma string e um número inteiro:");
+
+const alura = "Alura";
+
+console.log(alura * primeiroNumero);
