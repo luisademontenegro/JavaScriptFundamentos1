44032817cf99fbc78373460e98359e800d865cf1
Luísa de Montenegro <luisademontenegro@hotmail.com>
2022-01-09 02:57:47 -0300

    fundamentosStrings

---
+++ type-string.js
@@ -0,0 +1,21 @@
+const textoUm = "Olá, mundo!";
+
+const textoDois = 'Olá, mundo!';
+
+const senha = "SenhaSuperSegura456!";
+
+const StringDeNumeros = "34567";
+
+const citacao = "Meu nome é ";
+const meuNome = "Luísa";
+const outroNome = "Lídia";
+
+//concatenação: utilizando o símbolo + para unir fragmentos de textos ou palavras
+
+console.log(citacao + meuNome);
+
+console.log(citacao + outroNome);
+
+// Template string OU template literal: são strings que permitem expressões embutidas.
+// Você pode utilizar string multi-linhas e interpolação de string com elas.
+// Basicamente é uma nova forma de criar strings e tornar o seu código um pouco mais legível.
