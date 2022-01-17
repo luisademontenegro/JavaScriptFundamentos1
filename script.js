a6082a8bdf9934dd40388ce8d6bb09606e55fa2f
Luísa de Montenegro <luisademontenegro@hotmail.com>
2022-01-17 18:21:49 -0300

    truthyFalsy

---
+++ script.js
@@ -0,0 +1,36 @@
+//var
+
+// var altura = 5;
+// var comprimento = 7;
+
+// area = altura * comprimento;
+
+// console.log(area);
+
+//let
+
+// let forma = "Retângulo";
+// let altura2 = 8;
+// let comprimento2 = 3;
+// let area2;
+
+// if (forma === "Retângulo"){
+// area2 = altura2 * comprimento2;
+// } else {
+//     area2 = (altura2 * comprimento2)/2;
+// }
+
+// console.log(area2);
+
+const forma = 'Triângulo';
+const altura = 5;
+const comprimento = 7;
+let area;
+
+if (forma === 'Quadrado'){
+    area = altura * comprimento;
+} else {
+    area = (altura * comprimento)/2;
+}
+
+console.log(area);
---
+++ truthy-falsy.js
@@ -0,0 +1,24 @@
+//boolean
+
+const usuarioLogado = true;
+const contaPaga = false;
+
+//truthy ou flasy
+// zero --> false
+// um --> true
+
+console.log(0 == false);
+console.log(1 == true);
+console.log("" == false); //string vazia
+
+let minhaVar;
+let varNull = null;
+
+console.log(typeof minhaVar);
+console.log(typeof varNull);
+
+let numero = 3;
+let texto = 'Alura';
+
+console.log(typeof numero);
+console.log(typeof texto);
