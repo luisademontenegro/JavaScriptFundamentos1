c85ffb9ce849e38ac4ba28126d2d713f5e63aba6
Luísa de Montenegro <luisademontenegro@hotmail.com>
2022-01-17 11:47:16 -0300

    fundamentosBoolean

---
+++ type-boolean.js
@@ -0,0 +1,14 @@
+const primeiroNumero = 5;
+const segundoNumero = 10;
+
+console.log(primeiroNumero === segundoNumero);
+
+const terceiroNumero = 12;
+const quartoNumero = 12;
+
+console.log(terceiroNumero === quartoNumero);
+
+const texto1 = "Alura";
+const texto2 = "alura";
+
+console.log(texto1 === texto2);
