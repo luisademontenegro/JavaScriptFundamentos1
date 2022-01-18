60192fccac54be08b8d2a811e197d1d04b161176
Luísa de Montenegro <luisademontenegro@hotmail.com>
2022-01-18 19:07:23 -0300

    console

---
+++ console.js
@@ -0,0 +1,12 @@
+const minhaVar = true;
+
+console.log(245);
+console.log("texto");
+console.log(minhaVar);
+
+//tratamento de erro
+
+console.error('Erro encontrado');
+
+console.log("deu erro");
+console.error(new Error("deu erro"));
