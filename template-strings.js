5552e2a1dbb850b962f2cdd01dc4522f22f1ceda
Luísa de Montenegro <luisademontenegro@hotmail.com>
2022-01-18 20:46:29 -0300

    template-strings

---
+++ template-strings.js
@@ -0,0 +1,10 @@
+const nome = "Luísa";
+const idade = 2021-1991;
+const cidadeNascimento = "Fortaleza";
+
+// const apresentação = "Meu nome é " + nome + ", minha idade é "
+//  + idade + " e eu nasci em " + cidadeNascimento;
+
+const apresentação = `Meu nome é ${nome}, minha idade é ${idade} e eu nasci em ${cidadeNascimento}`;
+
+console.log(apresentação);
