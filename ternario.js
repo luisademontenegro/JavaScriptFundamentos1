7718931e82e64840d9dfea8d20337873b5d8e966
Luísa de Montenegro <luisademontenegro@hotmail.com>
2022-01-18 20:24:24 -0300

    ternário

---
+++ ternario.js
@@ -0,0 +1,10 @@
+const idadeMinima = 18;
+const idadeCliente = 12;
+
+// if (idadeCliente < idadeMinima){
+//     console.log('cliente não autorizado a ter acesso');
+// } else {
+//     console.log('acesso autorizado');
+// }
+
+console.log(idadeCliente < idadeMinima ? "cliente não autorizado a ter acesso" : "acesso autorizado" );
