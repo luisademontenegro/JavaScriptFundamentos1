42f2f41d216f0bca8aa47a687013c40b0b740b33
Luísa de Montenegro <luisademontenegro@hotmail.com>
2022-01-19 19:25:27 -0300

    funções

---
+++ funcoes.js
@@ -0,0 +1,19 @@
+// let x = "";
+// console.log(x);
+// x = "oi";
+
+
+//DECLARAÇÃO DE FUNÇÃO
+
+function imprimeTexto(texto) {
+    console.log(texto);
+}
+
+imprimeTexto("oi!");
+imprimeTexto("outro texto");
+
+function soma (){
+    return 2+2;
+}
+
+console.log(soma());
